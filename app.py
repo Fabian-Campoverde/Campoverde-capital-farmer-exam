f4296edbd9fe74fbfa215f1ef5d0364ba40ed2a2
FabianCv <fcampoverdev@unprg.edu.pe>
2025-05-26 17:09:40 -0500

    Creando formulario de cotizacion

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db' 
@@ -17,9 +20,19 @@ class Cotizacion(db.Model):
     precio = db.Column(db.Float, nullable=False)
     fecha = db.Column(db.DateTime, default=datetime.utcnow)
 
-
+# Crear Formulario con WTForms
+class CotizacionForm(FlaskForm):
+    nombre = StringField('Nombre del Cliente: ', validators=[DataRequired()])
+    email = StringField('Correo Electrónico: ', validators=[DataRequired(), Email()])
+    tipo_servicio = SelectField('Tipo de Servicio: ', choices=[
+        ('Constitución de empresa', 'Constitución de empresa'),
+        ('Defensa laboral', 'Defensa laboral'),
+        ('Consultoría tributaria', 'Consultoría tributaria')
+    ], validators=[DataRequired()])
+    descripcion = TextAreaField('Descripción del caso: ', validators=[DataRequired()])
+    submit = SubmitField('Generar Cotización')
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all() # Crear las tablas en la base de datos
+        db.create_all() # Crear la tabla en la base de datos
     app.run(debug=True)
